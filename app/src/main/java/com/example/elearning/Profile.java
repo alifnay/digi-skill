6b9aa3eb7c21f2a955fb84210efaec7845d34b5c
danendradipa <danendrach@gmail.com>
2024-12-01 01:09:23 +0700

    update profile can back to mainActivity

--- app/src/main/java/com/example/elearning/Profile.java
+++ app/src/main/java/com/example/elearning/Profile.java
@@ -1,11 +1,16 @@
 package com.example.elearning;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,7 +28,19 @@ public class Profile extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Back button
+        Button btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(v -> {
+            finish(); // Menutup ProfileActivity dan kembali ke aktivitas sebelumnya
+        });
 
         auth = FirebaseAuth.getInstance();
         firestore = FirebaseFirestore.getInstance();
